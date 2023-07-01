24283281d385a4ade4bbb776e79c42e9a6b064f8
Jan Kuzelik <honza.ku@gmail.com>
2023-07-01 10:47:40 +0200

    Added Svelte, created simple scrollbar

---
+++ src/components/Scrollbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let scrollY = 0
+  let innerHeight = 0
+  let offsetHeight = document.body.offsetHeight
+
+  function updateHeight() {
+    offsetHeight = document.body.offsetHeight
+  }
+</script>
+
+<svelte:window
+  bind:scrollY
+  bind:innerHeight
+  on:resize={updateHeight}
+/>
+
+<aside class="p-2 fixed left-0 top-0 h-screen">
+  <div
+    class="p-4 bg-rose-400"
+    style="height: {(100 * scrollY) / (offsetHeight - innerHeight)}%;"
+  />
+</aside>
