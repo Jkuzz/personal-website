de6e8eb5d1dfe17b0615435e31dc1ae3c6d277cb
Jan Kuzelik <honza.ku@gmail.com>
2023-07-04 15:17:56 +0200

    Added the images

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let url: string | undefined = undefined
+  export let action: (() => void) | undefined = undefined
+</script>
+
+{#if url}
+  <a href={url} target="_blank"><slot /></a>
+{/if}
