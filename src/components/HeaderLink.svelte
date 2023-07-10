803c49e4d3e35e24a27284792bdc0ac8f8241a67
Jan Kuzelik <honza.ku@gmail.com>
2023-07-11 00:25:35 +0200

    Added current section highlighting to header

--- src/components/HeaderLink.svelte
+++ src/components/HeaderLink.svelte
@@ -2,33 +2,48 @@
   import { tweened } from 'svelte/motion'
   import { cubicOut } from 'svelte/easing'
 
-  export let isCurrent = false
+  export let isActive = false
+  export let targetId: string
+
   let elementWidth: number
   const underlineWidth = tweened(0, { duration: 400, easing: cubicOut })
 
   function onMouseOver() {
-    if (!isCurrent) {
+    if (!isActive) {
       underlineWidth.set(elementWidth - 2)
     }
   }
 
   function onMouseOut() {
-    if (!isCurrent) {
+    if (!isActive) {
+      underlineWidth.set(0)
+    }
+  }
+
+  function scrollToTarget() {
+    console.log(document.querySelector(targetId))
+    document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  $: {
+    if (isActive) {
+      underlineWidth.set(elementWidth - 2)
+    } else {
       underlineWidth.set(0)
     }
   }
 </script>
 
-<div
+<button
   class="font-bold text-violet-900 cursor-pointer"
-  role="navigation"
   on:mouseenter={onMouseOver}
   on:mouseleave={onMouseOut}
   bind:clientWidth={elementWidth}
+  on:click={scrollToTarget}
 >
   <slot />
   <div
     style="width: {$underlineWidth}px;"
     class="h-[2px] rounded-sm bg-violet-900"
   />
-</div>
+</button>
--- src/components/PageHeader.svelte
+++ src/components/PageHeader.svelte
@@ -1,13 +1,50 @@
 <script lang="ts">
   import HeaderLink from './HeaderLink.svelte'
+
+  const sectionLinks = [
+    { id: 'section-hello', header: 'Hello' },
+    { id: 'section-tools', header: 'Tools' },
+    { id: 'section-projects', header: 'Projects' },
+    { id: 'section-contact', header: 'Contact' },
+  ]
+
+  let activeSectionId = 'section-hello'
+  let y = 0
+  let innerHeight: number
+  let offsetHeight = document.body.offsetHeight
+
+  $: {
+    const isEndOfPage = y + innerHeight - offsetHeight > -200
+    if (isEndOfPage) {
+      activeSectionId = sectionLinks[sectionLinks.length - 1].id
+    } else {
+      for (const section of sectionLinks) {
+        const offsetTop = document.getElementById(section.id)?.offsetTop
+        const isVisible = offsetTop != undefined && y >= offsetTop - 150
+        if (isVisible) {
+          activeSectionId = section.id
+          console.log('setting active: ' + section.header)
+        }
+      }
+    }
+  }
 </script>
+
+<svelte:window
+  bind:scrollY={y}
+  bind:innerHeight
+/>
 
 <div class="fixed top-0 z-50 flex h-10 w-screen flex-row items-stretch">
   <div class="w-[62vw] bg-violet-600" />
   <nav class="flex w-[38vw] px-4 flex-row items-center justify-end gap-4 bg-amber-300">
-    <HeaderLink>Hello</HeaderLink>
-    <HeaderLink>Tools</HeaderLink>
-    <HeaderLink>Projects</HeaderLink>
-    <HeaderLink>Contact</HeaderLink>
+    {#each sectionLinks as section}
+      <HeaderLink
+        targetId={section.id}
+        isActive={section.id === activeSectionId}
+      >
+        {section.header}
+      </HeaderLink>
+    {/each}
   </nav>
 </div>
