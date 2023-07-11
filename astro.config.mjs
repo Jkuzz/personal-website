ed007f04981ed6797d03423a38a14275eb449d29
Jan Kuzelik <honza.ku@gmail.com>
2023-07-11 21:03:44 +0200

    Test blank url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import svelte from '@astrojs/svelte'
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   site: 'https://jkuzz.github.io',
-  base: '/about-me',
+  base: '',
 })
