ac164bf08222ca63fa77b9b323863428f0e80954
Jan Kuzelik <honza.ku@gmail.com>
2023-07-11 21:00:50 +0200

    Changed url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import svelte from '@astrojs/svelte'
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   site: 'https://jkuzz.github.io',
-  base: '/personal-website',
+  base: '/about-me',
 })
