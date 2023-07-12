bd7b6901079df467cd969bbb5243b91a67c0958c
Jan Kuzelik <honza.ku@gmail.com>
2023-07-12 17:35:33 +0200

    Cleaned up email contact init

--- src/components/EmailDisplay.svelte
+++ src/components/EmailDisplay.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   import emailIcon from '../assets/icon-email.svg'
-  let emailText = 'wait-5-seconds-to-avoid-spambots'
+  let emailText = 'javascript:void(0)'
+  let anchor: HTMLAnchorElement
 
   window.setTimeout(() => {
-    emailText = atob('aG9uemEua3VAZ21haWwuY29t')
+    anchor.classList.remove('opacity-20', 'cursor-wait')
+    anchor.attributes.removeNamedItem('aria-disabled')
+    emailText = 'mailto:' + atob('aG9uemEua3VAZ21haWwuY29t')
   }, 5000)
 </script>
 
 <a
-  target="_blank"
-  href={'mailto:' + emailText}
-  class="origin-bottom-right transition-all hover:-translate-y-2 hover:rotate-6 flex items-center justify-center"
+  href={emailText}
+  class="origin-bottom-right transition-all flex items-center hover:-translate-y-2', 'hover:rotate-6 justify-center opacity-20 cursor-wait"
+  bind:this={anchor}
+  aria-disabled="true"
 >
   <img
     class="w-16"
